4f55159a6e61d151e70f798044866e622708f3ad
41324870 <41324870@087937-P33S105.macklabs.br>
2014-08-28 11:36:52 -0300

    teste php

--- ex41.php
+++ ex41.php
@@ -1,4 +1,28 @@
 <?php
+$p1=4.8;
+$m1=7;
+$m2=6;
+$pr1=3;
+$pr2=7;
+$pr3=8.3;
+$pr4=6.5;
+$proc=4.8;
+$tF=9;
+$pF=6;
 
-echo "ola";
+$mI=30*$p1+10*$m1+10*$m2+5*$pr1+5*$pr2+5*$pr3+5*$pr4+20*$proc+10*$tF;
+if($mI>=7.5){
+    $mF=$mI;
+    echo "Aprovado sem prova final.";
+}
+else{
+    $mF=($mI+$pF)/2;
+    if($mF>=6){
+        echo " Aprovado com prova final.";
+    }
+    else {
+        echo " Reprovado.";
+    }
+}
+
 ?>
